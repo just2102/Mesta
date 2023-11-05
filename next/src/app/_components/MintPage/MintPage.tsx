d182121e49236806e7e5af25edb0571f7a59278c
just2102 <thesixteensaltines@gmail.com>
2023-11-05 16:15:54 +0700

    Add useNetwork hook to MintPage component

--- next/src/app/_components/MintPage/MintPage.tsx
+++ next/src/app/_components/MintPage/MintPage.tsx
@@ -7,16 +7,23 @@ import styles from "./MintPage.module.scss";
 import Loading from "~/app/loading";
 import { ConnectWalletCta } from "../ConnectWalletCta/ConnectWalletCta";
 import { useCurrentCollectionData } from "~/app/api/web3/hooks/read/useCurrentCollectionData";
+import { useNetwork } from "wagmi";
 
 export function MintPage() {
+  const { chain } = useNetwork();
   const { collectionData, error, isError, isLoading } =
     useCurrentCollectionData();
+
+  const shouldDisplayConnectWalletCta =
+    !collectionData || isError || chain?.unsupported;
+  const shouldDisplayCurrentCollection =
+    collectionData && !error && !isLoading && !chain?.unsupported;
 
   return (
     <Box className={styles.mintPage}>
       {isLoading && <Loading />}
-      {isError && <ConnectWalletCta />}
-      {collectionData && (
+      {shouldDisplayConnectWalletCta && <ConnectWalletCta />}
+      {shouldDisplayCurrentCollection && (
         <>
           <CurrentCollection collectionData={collectionData} />
           <MintButton collectionAddress={collectionData.collectionAddress} />
