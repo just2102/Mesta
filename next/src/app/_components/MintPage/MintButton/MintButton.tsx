e273fb9f4ba99f38932f09455b726d9737f04e4b
Ilia Krasiuchenko <thesixteensaltines@gmail.com>
2024-06-27 19:51:12 +0700

    handle mint finished case for mintButton

--- next/src/app/_components/MintPage/MintButton/MintButton.tsx
+++ next/src/app/_components/MintPage/MintButton/MintButton.tsx
@@ -3,29 +3,43 @@
 import { Button } from "@mui/material";
 import styles from "./MintButton.module.scss";
 import { useAccount } from "wagmi";
+import { type CollectionData } from "~/app/api/web3/types/Collection";
 
 interface Props {
   mint: () => void;
   isSendingMintTx: boolean;
-  isWaitingMint: boolean;
+  collectionData: CollectionData;
 }
 
 export default function MintButton({
   mint,
   isSendingMintTx,
-  isWaitingMint,
+  collectionData,
 }: Props) {
   const { isConnected } = useAccount();
+
+  const mintFinished = collectionData.totalSupply === collectionData.maxSupply;
+
+  const getText = () => {
+    if (mintFinished) {
+      return "Mint is finished";
+    }
+
+    if (!isConnected) {
+      return "Connect Wallet";
+    }
+    return "Mint (Free)";
+  };
 
   return (
     <>
       <Button
-        disabled={isSendingMintTx || isWaitingMint || !isConnected}
+        disabled={isSendingMintTx || !isConnected || mintFinished}
         onClick={mint}
         className={styles.mintButton}
         variant="outlined"
       >
-        {isConnected ? "Mint (Free)" : "Connect Wallet"}
+        {getText()}
       </Button>
     </>
   );
